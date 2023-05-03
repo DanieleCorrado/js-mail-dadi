eb4864d4b648b117d774dc8a3ee9c24886d28af1
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-03 16:01:08 +0200

    improve js and add comment for mail check

--- Mail/js/index.js
+++ Mail/js/index.js
@@ -1,21 +1,31 @@
 // Inizializzo l'arrey
+
 const validEmail = ["prova@gmail.com", "daniele@virgilio.it", "test@test.it"]
-let validUserEmail;
+
+let validUserEmail = 0;
+
+// Chiedo all'utente di inserire la propria mail
 
 userEmail = prompt("Inserisci la tua Email:");
 
+// Esamino tutti gli elemeniti dell'array validEmail
+
 for (let i = 0; i < validEmail.length; i++) {
+
+  // Controllo se l'i-esimo elemento dell'array corrsponde alla mail inserita dall'utente
 
   if( validEmail[i] === userEmail) {
 
     validUserEmail = 1;
+    console.log("Puoi accedere all'evento");
+    i = validUserEmail.length;
 
   }
 
 }
 
-if (validUserEmail == 1) {
-  console.log("Puoi accedere all'evento");
-} else {
+if (validUserEmail === 0) {
+
   console.log("Non puoi accedere all'evento");
+
 }
