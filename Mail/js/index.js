17be7355187d5022057dcbd86ed448fe7f2fd7a6
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-03 15:56:40 +0200

    Revert "improve js and add comment"
    This reverts commit f6d2ad1796866036496a52150b7e7f5cde736a6a.

--- Mail/js/index.js
+++ Mail/js/index.js
@@ -1,31 +1,21 @@
 // Inizializzo l'arrey
-
-const validEmail = ["prova@gmail.com", "daniele@virgilio.it", "test@test.it"];
-
-let validUserEmail = 0;
-
-// Richedo l'inserimento della mail all'utente
+const validEmail = ["prova@gmail.com", "daniele@virgilio.it", "test@test.it"]
+let validUserEmail;
 
 userEmail = prompt("Inserisci la tua Email:");
 
-// Scorro tutti i valorori di validEmail
-
 for (let i = 0; i < validEmail.length; i++) {
-
-  // Controllo se l'i-esimo elemento dell'arrey corrisponde all'email inserita dal'utente
 
   if( validEmail[i] === userEmail) {
 
     validUserEmail = 1;
-    console.log("Puoi accedere all'evento");
-    i = validEmail.length;
+
   }
 
 }
 
-
-if (validUserEmail === 0) {
-
+if (validUserEmail == 1) {
+  console.log("Puoi accedere all'evento");
+} else {
   console.log("Non puoi accedere all'evento");
-
 }
