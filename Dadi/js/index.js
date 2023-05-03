00796c9f5c44940200e84bf68ab5ddd54fe3d1e0
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-03 16:15:22 +0200

    Add console dice roll

--- Dadi/js/index.js
+++ Dadi/js/index.js
@@ -1,1 +1,23 @@
-console.log("prova");
+// genero il risultato del dado per l'utente
+
+const userDiceRoll = (Math.floor(Math.random() * 6) + 1);
+
+// Genero il risultato del dado per il computer
+
+const computerDiceRoll = (Math.floor(Math.random() * 6) + 1);
+
+console.log(`Il risultato dell'utente è: ${userDiceRoll}, mentre il risultato del computer è: ${computerDiceRoll}`);
+
+// Controllo quale dei due valori è maggiore per decretare il vincitore
+
+if (userDiceRoll > computerDiceRoll) {
+
+  console.log("L'utente ha vinto");
+
+} else if (computerDiceRoll > userDiceRoll){
+
+  console.log("Il computer ha vinto");
+
+} else {
+  console.log("C'è stato un pareggio");
+}
