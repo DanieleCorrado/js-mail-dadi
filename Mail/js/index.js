57d38b480fb997a81e32c6e0b133b7065615de33
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-03 15:49:13 +0200

    add console mail check

--- Mail/js/index.js
+++ Mail/js/index.js
@@ -1,1 +1,21 @@
-console.log("prova");
+// Inizializzo l'arrey
+const validEmail = ["prova@gmail.com", "daniele@virgilio.it", "test@test.it"]
+let validUserEmail;
+
+userEmail = prompt("Inserisci la tua Email:");
+
+for (let i = 0; i < validEmail.length; i++) {
+
+  if( validEmail[i] === userEmail) {
+
+    validUserEmail = 1;
+
+  }
+
+}
+
+if (validUserEmail == 1) {
+  console.log("Puoi accedere all'evento");
+} else {
+  console.log("Non puoi accedere all'evento");
+}
